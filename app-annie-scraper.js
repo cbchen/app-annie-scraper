9502928d21f09668387e512ee92e8051dd33482d
Chris Chen <chris.bing@gmail.com>
2013-08-06 16:28:58 +0000

    refactored scrape function

--- app-annie-scraper.js
+++ app-annie-scraper.js
@@ -1,10 +1,43 @@
 // Script to scrape App Annie rankings
 // author: chris
 
-var page = require('webpage').create(),
-    fs = require('fs');
+function scrape_page_to_file(page) {
+    page.evaluate(function() {
+	// helper function to format date as YYYY-mm-dd
+	var format_date = function(date) {
+	    var month = date.getMonth(), 
+	    day = date.getDate();
+	    return(date.getFullYear() + '-' + (month >= 10 ? month : '0' + month) + '-' + (day >= 10 ? day : '0' + day));
+	};
+	var formatted_date = format_date(new Date($('span.date').text()));
+	
+	// Pull the different app categories from the table header
+	var app_types = $('div.head-helper th div').map(function(index, element) {
+	    return $(element).text();
+	}).get();
+	
+	// Process the rankings row by row
+	$('#storestats-top-table tr').each(function(rank) {
+	    rank++;  // add 1 because rank is indexed at 0
+	    
+	    // Within each row, find all td elements of class=app and pull out the app info
+	    $(this).find('td.app').each(function(index) {
+		console.log(rank + '\t' + $(this).find('span.app-name').attr('title').trim() + 
+			    '\t' + $(this).find('span.app-pub-er').attr('title').trim() + 
+			    '\t' + app_types[index] + '\t' + formatted_date);
+	    });
+	});
+    });
+    
+    outfile.flush();
+    outfile.close();   
+}
 
-var outfile = fs.open('OUTPUT.tsv', 'w');
+// MAIN SCRIPT
+page = require('webpage').create(),
+fs = require('fs');
+
+outfile = fs.open('OUTPUT.tsv', 'w');
 outfile.writeLine('ranking\tapp\tdeveloper\tranking_type\treport_date'); // write headers
 
 // Catch console messages and write to file, as that's how we'll get data out of the page
@@ -22,35 +55,8 @@ page.open('http://www.appannie.com/top/', function(status) {
 	});
 	
 	window.setTimeout(function() {  // set timeout to allow additional apps to load
-	    page.evaluate(function() {
-		// helper function to format date as YYYY-mm-dd
-		var format_date = function(date) {
-		    var month = date.getMonth(), 
-		        day = date.getDate();
-		    return(date.getFullYear() + '-' + (month >= 10 ? month : '0' + month) + '-' + (day >= 10 ? day : '0' + day));
-		};
-		var formatted_date = format_date(new Date($('span.date').text()));
-		
-		// Pull the different app categories from the table header
-		var app_types = $('div.head-helper th div').map(function(index, element) {
-		    return $(element).text();
-		}).get();
-		
-		// Process the rankings row by row
-		$('#storestats-top-table tr').each(function(rank) {
-		    rank++;  // add 1 because rank is indexed at 0
-
-		    // Within each row, find all td elements of class=app and pull out the app info
-		    $(this).find('td.app').each(function(index) {
-			console.log(rank + '\t' + $(this).find('span.app-name').attr('title').trim() + 
-				    '\t' + $(this).find('span.app-pub-er').attr('title').trim() + '\t' + app_types[index] + '\t' + formatted_date);
-		    });
-		});
-	    });
-	    
-	    outfile.flush();
-	    outfile.close();
+	    scrape_page_to_file(page);
 	    phantom.exit();
 	}, 2000);
     }
-})
+});
