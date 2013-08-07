dc3c3e351f91505c0ca2bf5bea15e0dfca1dbec4
Chris Chen <chris.bing@gmail.com>
2013-08-07 07:38:20 +0000

    generalized script, require url and outputfile as parameters

--- app-annie-scraper.js
+++ app-annie-scraper.js
@@ -1,7 +1,7 @@
 // Script to scrape App Annie rankings
 // author: chris
 
-function scrape_page_to_file(page) {
+function scrape_rankings_to_file(page) {
     page.evaluate(function() {
 	// helper function to format date as YYYY-mm-dd
 	var format_date = function(date) {
@@ -9,7 +9,7 @@ function scrape_page_to_file(page) {
 	    day = date.getDate();
 	    return(date.getFullYear() + '-' + (month >= 10 ? month : '0' + month) + '-' + (day >= 10 ? day : '0' + day));
 	};
-
+	
 	// Pull the different app categories from the table header
 	var app_types = $('div.head-helper th div').map(function(index, element) {
 	    return $(element).text();
@@ -21,7 +21,7 @@ function scrape_page_to_file(page) {
 	    
 	    // Within each row, find all td elements of class=app and pull out the app info
 	    $(this).find('td.app').each(function(index) {
-
+		
 		var app_data = [ranking];  // initialize with ranking
 		app_data.push($(this).find('span.app-name').attr('title').trim());  // app_name
 		app_data.push($(this).find('span.app-pub-er').attr('title').trim());  // publisher
@@ -29,7 +29,7 @@ function scrape_page_to_file(page) {
 		app_data.push($(this).find('span.var').text().trim());  // ranking_change
 		app_data.push($(this).hasClass('has_iap') ? 1 : 0);  // has_in_app_purchases
 		app_data.push(format_date(new Date($('span.date').text())));  // report_date
-
+		
 		console.log(app_data.join('\t'));
 	    });
 	});
@@ -41,10 +41,17 @@ function scrape_page_to_file(page) {
 
 // MAIN SCRIPT
 
+var args = require('system').args;
+if (args.length !== 3) {
+    console.log('usage: phantomjs app-annie-scraper.js <url> <output_filename>');
+    phantom.exit();
+}
+var url = args[1], output_filename = args[2];
+
 var headers = ['ranking', 'app_name', 'publisher', 'ranking_type', 'ranking_change', 'has_in_app_purchases', 'report_date'];
 
 var fs = require('fs');
-outfile = fs.open('OUTPUT.tsv', 'w');
+outfile = fs.open(output_filename, 'w');
 outfile.writeLine(headers.join('\t'));
 
 var page = require('webpage').create();
@@ -54,7 +61,7 @@ page.onConsoleMessage = function(msg) {
     outfile.writeLine(msg);
 };
 
-page.open('http://www.appannie.com/top/', function(status) {
+page.open(url, function(status) {
     if (status !== 'success') {
 	console.log('could not retrieve page');
 	phantom.exit();
@@ -64,7 +71,7 @@ page.open('http://www.appannie.com/top/', function(status) {
 	});
 	
 	window.setTimeout(function() {  // set timeout to allow additional apps to load
-	    scrape_page_to_file(page);
+	    scrape_rankings_to_file(page);
 	    phantom.exit();
 	}, 2000);
     }
