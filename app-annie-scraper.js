3a397f526a6fe0554c8f7e11ca5f1442141fbc63
Chris Chen <chris.bing@gmail.com>
2013-08-08 19:51:42 +0000

    allow scraping multiple pages

--- app-annie-scraper.js
+++ app-annie-scraper.js
@@ -9,7 +9,7 @@ function scrape_rankings_to_file(page) {
 	    day = date.getDate();
 	    return(date.getFullYear() + '-' + (month >= 10 ? month : '0' + month) + '-' + (day >= 10 ? day : '0' + day));
 	};
-	
+
 	// Pull the different app categories from the table header
 	var app_types = $('div.head-helper th div').map(function(index, element) {
 	    return $(element).text();
@@ -17,62 +17,80 @@ function scrape_rankings_to_file(page) {
 	
 	// Process the rankings row by row
 	$('#storestats-top-table tr').each(function(ranking) {
-	    ranking++;  // add 1 because ranking is indexed at 0
-	    
+	    ranking++;  // add 1 because var ranking starts at 0;
+
 	    // Within each row, find all td elements of class=app and pull out the app info
 	    $(this).find('td.app').each(function(index) {
-		
-		var app_data = [ranking];  // initialize with ranking
-		app_data.push($(this).find('span.app-name').attr('title').trim());  // app_name
-		app_data.push($(this).find('span.app-pub-er').attr('title').trim());  // publisher
-		app_data.push(app_types[index]);  // ranking_type
-		app_data.push($(this).find('span.var').text().trim());  // ranking_change
-		app_data.push($(this).hasClass('has_iap') ? 1 : 0);  // has_in_app_purchases
-		app_data.push(format_date(new Date($('span.date').text())));  // report_date
+		var td_elem = $(this);
+		var app_data = [
+		    ranking,  // app_ranking
+		    td_elem.find('span.app-name').attr('title').trim(),  // app_name
+		    td_elem.find('span.app-pub-er').attr('title').trim(),  // publisher
+		    app_types[index],  // ranking_type
+		    td_elem.find('span.var').text().trim(),  // ranking_change
+		    td_elem.hasClass('has_iap') ? 1 : 0,  // has_in_app_purchases
+		    format_date(new Date($('span.date').text()))  // report_date
+		];  
 		
 		console.log(app_data.join('\t'));
 	    });
 	});
-    });
-    
-    outfile.flush();
-    outfile.close();   
+    });    
 }
 
 // MAIN SCRIPT
+var scrapeJobs = [
+    { url: 'http://www.appannie.com/top/iphone/united-states/', hasBeenProcessed: false, outputFile: 'output/iphone' }, 
+    { url: 'http://www.appannie.com/top/ipad/united-states/', hasBeenProcessed: false, outputFile: 'output/ipad' },
+    { url: 'http://www.appannie.com/top/android/united-states/', hasBeenProcessed: false, outputFile: 'output/android' },
+    { url: 'http://www.appannie.com/top/windows/united-states/', hasBeenProcessed: false, outputFile: 'output/windows' }
+];
 
-var args = require('system').args;
-if (args.length !== 3) {
-    console.log('usage: phantomjs app-annie-scraper.js <url> <output_filename>');
+process_next();
+// END MAIN SCRIPT
+
+
+function process_next() {
+    for (var i = 0; i < scrapeJobs.length; i++) {
+	var scrapeJob = scrapeJobs[i];
+	if (scrapeJob.hasBeenProcessed) continue;
+
+	console.log('processing url: ' + scrapeJob.url);
+	scrapeJob.hasBeenProcessed = true;
+	return process_url(scrapeJob.url, scrapeJob.outputFile, process_next);
+    }
     phantom.exit();
 }
-var url = args[1], output_filename = args[2];
 
-var headers = ['ranking', 'app_name', 'publisher', 'ranking_type', 'ranking_change', 'has_in_app_purchases', 'report_date'];
 
-var fs = require('fs');
-outfile = fs.open(output_filename, 'w');
-outfile.writeLine(headers.join('\t'));
+function process_url(url, outputFile, callback) {
+    var fs = require('fs');
+    var outfile = fs.open(outputFile, 'w');
 
-var page = require('webpage').create();
-
-// Catch console messages and write to file, as that's how we'll get data out of the page
-page.onConsoleMessage = function(msg) {
-    outfile.writeLine(msg);
-};
-
-page.open(url, function(status) {
-    if (status !== 'success') {
-	console.log('could not retrieve page');
-	phantom.exit();
-    } else {
+    var headers = ['ranking', 'app_name', 'publisher', 'ranking_type', 'ranking_change', 'has_in_app_purchases', 'report_date'];
+    outfile.writeLine(headers.join('\t'));
+    
+    var page = require('webpage').create();
+    
+    // Catch console messages and write to file, as that's how we'll get data out of the page
+    page.onConsoleMessage = function(msg) {
+	outfile.writeLine(msg);
+    };
+    
+    page.open(url, function(status) {
+	if (status !== 'success') {
+	    return console.log('could not retrieve page');
+	}
 	page.evaluate(function() {
 	    $('a.load-all').click();  // load all of the apps by clicking the anchor
 	});
 	
 	window.setTimeout(function() {  // set timeout to allow additional apps to load
 	    scrape_rankings_to_file(page);
-	    phantom.exit();
+	    outfile.flush();
+	    outfile.close();
+
+	    return callback();
 	}, 2000);
-    }
-});
+    });
+};
